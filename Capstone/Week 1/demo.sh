10faf94b2c1f8cd529f92ada7ee7c41ce528a080
Samuel Fincher <smfincher42@tntech.edu>
2026-04-13 14:44:59 -0500

    Made demo.sh wait for pip.

--- Capstone/Week 1/demo.sh
+++ Capstone/Week 1/demo.sh
@@ -6,6 +6,10 @@ then
 	echo 'Failed to start docker images. Aborting demo...'
 	exit 2
 fi
+echo 'Waiting for pip...'
+docker exec bob_node pip install cryptography > /dev/null
+docker exec alice_node pip install cryptography > /dev/null
+
 echo 'Starting Server...'
 docker exec bob_node python server.py > server.log &
 sleep 1
