5794b21917520a7876a6932820b5239fa7f0758a
Samuel Fincher <smfincher42@tntech.edu>
2026-04-13 15:33:34 -0500

    Added tcpdump to demo.

--- Capstone/Week 1/demo.sh
+++ Capstone/Week 1/demo.sh
@@ -6,21 +6,22 @@ then
 	echo 'Failed to start docker images. Aborting demo...'
 	exit 2
 fi
+echo initializing tcpdump...
+# killing tcpdump is not required since it will exit when br-crypto disappears. sudo -b makes tcpdump run in background.
+sudo -b tcpdump -i 'br-crypto' -w 'demo.pcap' -f 'tcp port 65432'
 echo 'Waiting for pip...'
 docker exec bob_node pip -q install cryptography &> /dev/null
 docker exec alice_node pip install cryptography &> /dev/null
-
 echo 'Starting Server...'
 docker exec bob_node python server.py > server.log &
 sleep 1
-echo 'Running Client...'
+echo 'Sending Message...'
 docker exec alice_node python client.py > client.log
 docker compose down
 echo 'SERVER OUTPUT:'
 cat ./server.log
 echo 'CLIENT OUTPUT:'
 cat ./client.log
+
 rm ./server.log
 rm ./client.log
-
-
