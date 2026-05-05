85cf57a2489743d43750ad41c3a5ab13c01a6a0b
SammyD2002 <35823092+SammyD2002@users.noreply.github.com>
2026-05-05 08:49:21 -0500

    Made use of CLIENT and SERVER variables in demo.sh

--- Capstone/demo.sh
+++ Capstone/demo.sh
@@ -6,10 +6,19 @@ if [ $# -eq 1 ]
 then
 	CLIENT="$1"
 fi
+# Ensure client is a container path, not a local path
+CLIENT=$(realpath --relative-to ./ "$CLIENT")
 if [ $# -eq 2 ]
 then
 	SERVER="$2"
 fi
+# Ensure server is a container path, not a local path
+SERVER=$(realpath --relative-to ./ "$SERVER")
+# Set log files based on server and client python script names
+SRVLOG="$(realpath $(basename -s '.py' $SERVER).log)"
+SRVERR="$(realpath $(basename -s '.py' $SERVER).err.log)"
+CLILOG="$(realpath $(basename -s '.py' $CLIENT).log)"
+CLIERR="$(realpath $(basename -s '.py' $CLIENT).err.log)"
 # Start both docker images
 sudo docker compose up -d
 if [ $? -ne 0 ]
@@ -28,7 +37,7 @@ sudo docker exec alice_node pip -q install cryptography &> /dev/null
 SRVEXIT=-1
 CLIEXIT=-1
 do_srv () {
-sudo docker exec bob_node python server.py 2> server.err.log 1> server.log
+sudo docker exec bob_node python "$SERVER" 2> "$SRVERR" 1> "$SRVLOG"
 SRVEXIT=$?
 echo "DONE"
 echo "BOB: SERVER EXITTED $SRVEXIT
@@ -36,7 +45,7 @@ SERVER OUTPUT:
 $(cat ./server.log)
 SERVER ERRORS:
 $(cat ./server.err.log)"
-sudo rm -r ./server.log
+sudo rm ./server.log
 }
 echo -e "\r"'Executing Demo Transmission:'
 echo '    Starting Server...'
@@ -44,15 +53,15 @@ do_srv &
 srvjob=$!
 sleep 1
 echo '    Sending Message...'
-sudo docker exec alice_node python client.py 2> client.err.log 1> client.log
+sudo docker exec alice_node python "$CLIENT" 2> "$CLIERR" 1> "$CLILOG"
 CLIEXIT=$?
 wait $srvjob
 echo "ALICE: CLIENT EXITTED $CLIEXIT
 CLIENT OUTPUT:
-$(cat ./client.log)
+$(cat "$CLILOG")
 CLIENT ERRORS:
-$(cat ./client.err.log)"
-sudo rm -r ./client.log
+$(cat "$CLIERR")"
+sudo rm "$CLILOG"
 echo "Shutting Down Containers..."
 sudo docker compose down
 wait "$TEEPID"
